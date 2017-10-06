ead1c3fc7464215d9855cb00414a600f52cba14a
Blydro Klonk <blydro@gmail.com>
2017-10-07 01:42:34 +0200

    some small fixes to make it cleaner

--- src/App.js
+++ src/App.js
@@ -1,58 +1,7 @@
 import React, {Component} from 'react';
-import io from 'socket.io-client';
-import Peer from 'simple-peer';
 
+import {setupPeers, massSend} from './networking'
 import LogItem from './components/LogItem';
-
-const peers = {};
-const socket = io('http://localhost:3030');
-
-function setupPeers(cb, logger) {
-	socket.on('connect', () => {
-		logger('Connected to signalling server, Peer ID: '+ socket.id);
-	});
-
-	socket.on('peer', data => {
-		const peerId = data.peerId;
-		const peer = new Peer({initiator: data.initiator, trickle: false});
-
-		logger('Peer available for connection discovered from signalling server, Peer ID: '+ peerId);
-
-		socket.on('signal', data => {
-			if (data.peerId === peerId) {
-				logger('Received signalling data', data, 'from Peer ID:'+ peerId);
-				peer.signal(data.signal);
-			}
-		});
-
-		peer.on('signal', data => {
-			logger('Advertising signalling data', data, 'to Peer ID:'+ peerId);
-			socket.emit('signal', {
-				signal: data,
-				peerId
-			});
-		});
-		peer.on('error', e => {
-			console.log('Error sending connection to peer %s:', peerId, e);
-		});
-		peer.on('connect', () => {
-			logger('Peer connection established');
-			// Peer.send('hey peer');
-		});
-		peer.on('data', data => {
-			logger('Recieved data from peer:'+ data);
-			cb(data);
-		});
-		peers[peerId] = peer;
-	});
-}
-
-function massSend(peers, message) {
-	// eslint-disable-next-line
-	Object.keys(peers).map(peer => {
-		peers[peer].send(message);
-	});
-}
 
 class App extends Component {
 
@@ -82,9 +31,9 @@ class App extends Component {
 	}
 
 	massTextBootyCall() {
-		this.logger('mass texting ', this.message.value);
+		this.logger('mass texting '+ this.message.value);
 
-		massSend(peers, this.message.value);
+		massSend(this.message.value);
 	}
 
 	render() {
---
+++ src/networking.js
@@ -0,0 +1,55 @@
+import io from 'socket.io-client';
+import Peer from 'simple-peer';
+
+const peers = {};
+
+const ioId = process.env.IOID || 'http://localhost:3030';
+const socket = io(ioId);
+
+function setupPeers(cb, logger) {
+	socket.on('connect', () => {
+		logger('Connected to signalling server, Peer ID: ' + socket.id);
+	});
+
+	socket.on('peer', data => {
+		const peerId = data.peerId;
+		const peer = new Peer({initiator: data.initiator, trickle: false});
+
+		logger('Peer available for connection discovered from signalling server, Peer ID: ' + peerId);
+
+		socket.on('signal', data => {
+			if (data.peerId === peerId) {
+				logger('Received signalling data', data, 'from Peer ID:' + peerId);
+				peer.signal(data.signal);
+			}
+		});
+
+		peer.on('signal', data => {
+			logger('Advertising signalling data', data, 'to Peer ID:' + peerId);
+			socket.emit('signal', {
+				signal: data,
+				peerId
+			});
+		});
+		peer.on('error', e => {
+			console.log('Error sending connection to peer %s:', peerId, e);
+		});
+		peer.on('connect', () => {
+			logger('Peer connection established');
+			// Peer.send('hey peer');
+		});
+		peer.on('data', data => {
+			logger('Recieved data from peer:' + data);
+			cb(data);
+		});
+		peers[peerId] = peer;
+	});
+}
+
+function massSend(message) {
+	Object.keys(peers).map(peer => {
+		peers[peer].send(message);
+	});
+}
+
+export {setupPeers, massSend};
